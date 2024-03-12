cd495d575cfdc0e96284cf8b1e132217494323c7
Ulises Justo Saucedo <ulisesjustosaucedo@gmail.com>
2024-03-12 17:43:20 -0300

    First state of the project

---
+++ app/src/main/java/com/example/datatestlab/AuthActivity.kt
@@ -0,0 +1,17 @@
+package com.example.datatestlab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
